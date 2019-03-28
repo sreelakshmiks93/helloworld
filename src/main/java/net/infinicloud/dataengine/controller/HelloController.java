9d872f146cea91f1a2d46adfce0ddc6227a54446
sreelakshmiks93 <31011735+sreelakshmiks93@users.noreply.github.com>
2019-03-28 07:52:59 +0530

    Add files via upload

---
+++ src/main/java/net/infinicloud/dataengine/controller/HelloController.java
@@ -0,0 +1,20 @@
+package net.infinicloud.dataengine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		String name = "test";
+		return "hello";
+
+	}
+ 
+}
